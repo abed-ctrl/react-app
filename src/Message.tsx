8668d21104cf470221fd2780593da497abfd2978
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-03-19 00:14:13 +0000

    Adding a Behaviour to Message Component

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,12 @@
 //PascalCasing
 function Message() {
     //JSX Javascript XML Syntax
-    return <h1>Hello World</h1>;
+    //expression => is a value or a piece of code that return a value 
+    const name = 'Cleverali';
+    if (name)
+        return <h1>Hello { name }</h1>;
+    else
+        return <h1>Hello World</h1>
 }
 
 export default Message;
