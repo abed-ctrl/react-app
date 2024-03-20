c4f88031c4c178471c2d0cba5e8c6fa759389143
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-03-20 23:53:28 +0000

    Fragments

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,44 @@
+//import { Fragment } from "react";
+
 function ListGroup() {
   //class is a reserved word in React and Typescript => className
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    //In React we can not return more than one Element
+    //<h1>List</h1>// React.CreateElement('h1')
+    // Solution One: Wrap the 2 or more Element in One Element Example <div></div>
+    // but using this solution we will add an unnecessary Element.
+    /*<div>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </div>*/
+    // Solution Two: Use Fragment. no other Element added
+    /*<Fragment>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </Fragment>*/
+    // Short Solution to use Fragment <></>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
   );
 }
 
