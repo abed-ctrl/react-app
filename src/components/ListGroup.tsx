46567c00f0460b29f9408646ab54657edf775bb7
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-03-22 02:28:28 +0000

    Managing State

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,23 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
-  // Handling Events
+  // Managing state
   let items = ["Casablanca", "Oued-Zem", "Essaouira", "Tantan", "Meknes"];
 
-  // In React Each Element have a property onClick to handle Click Event on Element.
-  // Print Item Clicked to the Console. using item or items[index]
-  // To see the event class we add event to the arrow function param and log it to console
+  //let selectedIndex = 0;
+  // we want to add active class dynamically to <li> Element went user click on it
 
-  // we use typescript we should give type of event parameter.
-  // type annotation in typeScript => event: MouseEvent
-  // with typescript we get auto-completion and type safety
-  // Event Handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // Hook = is a function that allows us to tap into built-in features in React.
+  // we never touch the DOM. We just update the state and React Update DOM.
+  //const arr = useState(-1);
+  //arr[0]; //variable (selectedIndex)
+  //arr[1]; //updater function
+
+  // this a React convention
+  // we have now a state variable called selectedIndex and a updater function called setSelectedIndex
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // every component will have his own state
 
   return (
     <>
@@ -21,14 +26,20 @@ function ListGroup() {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            //first method
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={index}
-            //onClick={() => console.log(item, index)}
-            // this logic will be more complex this is why we use function to handle event
-            //onClick={(event) => console.log(event)}
-            // we don't call the function handleClick() we just pass a reference.
-            // so this function will be called later on run time.
-            onClick={handleClick}
+            // we cant use here a local variable selectedIndex
+            // we should tell React that this component is going to have data or state that may change over time
+            // this is why we should use useState() built-in function.
+            // onClick={() => (selectedIndex = index)}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
