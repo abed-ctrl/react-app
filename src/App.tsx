ad154ce99a053ca6f1cbbcba9df8455a82b2ec31
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-03-27 02:12:31 +0000

    04-Styling Component - Adding Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
-// 04 - Styling Component => Popular UI LIBRARIES
+// 04 - Styling Component => Adding Icons
 
-// There are several UI libraries available that can assist us in quickly building beautiful
-// and modern applications. Some popular options include Bootstrap, Material UI,
-// TailwindCSS, DaisyUI, ChakraUI, and more.
-// - Bootstrap
-// - Material UI : google material designs is the design language used in google product
-// - Tailwind CSS: a utility-first css framework
-//          - Daisy UI : instead of using utility class we can use component like "alert"
-//          - Chakra UI : it's similar to Material UI. React component library that's build in top of tailwind
+// We can add icons to our applications using the react-icons library.
+// npm i react-icons@4.7.1
 
-import ListGroup from "./components/ListGroup";
+import { BsCalendarFill } from "react-icons/bs";
 import "./App.css";
 
 function App() {
-  const heading = "Cities";
-  const items = ["Casablanca", "Oued-Zem", "Essaouira"];
-  const handleOnSelectedItem = (item: string) => console.log(item);
-
   return (
     <div>
-      <ListGroup
-        heading={heading}
-        items={items}
-        onSelectedItem={handleOnSelectedItem}
-      />
+      <BsCalendarFill size="60px" color="blue" />
     </div>
   );
 }
