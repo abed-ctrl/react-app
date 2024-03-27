529fb1e9c7c5ada1fb05e1edccc40515676cf6ed
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-03-27 01:36:12 +0000

    04-Styling Component - Separation of Concerns

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-// 04 - Styling Component => CSS-IN-JS
+// 04 - Styling Component => Separation of Concerns
 
-// Another approach for styling our component called CSS-IN-JS.
-// With CSS-in-JS, we define all the styles for a component alongside its code. Like CSS
-// modules, this provides scoping for CSS classes and eliminates conflicts. It also makes it
-// easier for us to change or delete a component without affecting other components.
-// and it's easy to style based on props/state.
-// this is some libraries that implement this concept: - styled component - emotion - polished
+// Separation of Concerns is an important design principle in software engineering.
+// Separation of Concerns recommend that's we should divide a program into distinct sections where
+// each section handles a specific functionality, rather than having everything in one place.
+// with this our program will be:
+// - Modular
+// - Easier to Understand
+// - Easier to Maintain
+// - Easier to Modify
+// modularity provides a number of benefits:
+// - if our Programs are modular we can build and test these modules independently and reuse them in other Programs.
+// - every module is responsible of a single Concern
+// - in a module all complexity and implementation details are hidden behind a well defined Interface
+
+// Separation of concerns is not just about organizing code into files, but rather dividing
+// areas of functionality. Therefore, CSS-in-JS does not violate the separation of concerns
+// principle as all the complexity for a component remains hidden behind its interface.
 
 import ListGroup from "./components/ListGroup";
 import "./App.css";
@@ -15,6 +25,8 @@ function App() {
   const items = ["Casablanca", "Oued-Zem", "Essaouira"];
   const handleOnSelectedItem = (item: string) => console.log(item);
 
+  // in this case all the complexity around the ListGroup Component is hidden behind a well defined Interface
+  // the consumer of this component doesn't care what's behind this interface. doesn't care if we put everything in one file or multiple files
   return (
     <div>
       <ListGroup
