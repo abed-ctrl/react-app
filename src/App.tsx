fb82bc12d8b77aa9d0dc505d6a56a5d1a231d979
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-03-30 02:46:40 +0000

    05 - Managing Component State - Keeping Components Pure

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,31 @@
-// 05 - Managing Component State => Choosing the State Structure
+// 05 - Managing Component State => Keeping Components Pure
+// A pure function is one that always returns the same result given the same input.
+// Pure functions should not modify objects outside of the function.
+// example : const result = myFunc(1); if this function return 'a' it should return a every time is called
+// if it's return 'b' or 'c'. it's not called a Pure function
+// React is designed around this concept
+// React expects our function components to be pure. A pure component should always
+// return the same JSX given the same input.
+//
+// props      -> { Component }    -> JSX
+// same props                        same JSX
+// this is for performance reasons.
+// React will skip re-rendering if the inputs of the component haven't change.
+//
+// To keep our components pure, we should avoid making changes during the render phase.
 
-import { useState } from "react";
+import Message from "./Message";
 
 function App() {
-  // const [firstName, setFirstName] = useState("Ali");
-  // const [lastName, setLastName] = useState("OD");
-  // if we want to show the fullName. we don't need to use another State fullName because this will be redundant.
-  // we can just use it like this.
-  //const fullName = firstName + " " + lastName;
-  // sometimes our state variables are related, in those cases is better to group them in a single object.
-  const [isLoading, setLoading] = useState(false);
-  // the first and second state variables are related. we can create an object person and wrap theme into it.
-  // we will comment fist and second state variables here and the fullName variable too.
-
-  const [person, setPerson] = useState({
-    firstName: "Ali",
-    lastName: "OD",
-    // we should avoid deeply nested structure like this.
-    contact: {
-      address: {
-        street: "",
-      },
-    },
-    // because is difficult to update an object with this structure.
-    // we prefer a flat structure.
-    // Best Practices :
-    // - Avoid redundant state variables
-    // - Group related variables inside an object
-    // - Avoid deeply nested structures
-  });
-
   return (
     <div>
-      {/* without variable */}
-      {/* firstName} {lastName*/}
-      <br />
-      {/* with variable*/}
-      {/* fullName */}
-      {/* object variable*/}
-      {person.firstName} {person.lastName}
+      {/* here we didn't use the count variable in Message. result 0 0 0 */}
+      {/* if we increment the count variable count++ inside our component we get a different results every time we render it. result 2 4 6 */}
+      {/* To keep our components pure, we should avoid making changes during the render phase. */}
+      {/* if we declare the count variable inside our component it will be pure. */}
+      <Message />
+      <Message />
+      <Message />
     </div>
   );
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,12 +1,10 @@
-//PascalCasing
-function Message() {
-    //JSX Javascript XML Syntax
-    //expression => is a value or a piece of code that return a value 
-    const name = 'Cleverali';
-    if (name)
-        return <h1>Hello { name }</h1>;
-    else
-        return <h1>Hello World</h1>
-}
+// if we declare a variable out of our component and use it inside it. our component is not pure.
+// let count = 0;
+
+const Message = () => {
+  let count = 0;
+  count++;
+  return <div>Message {count}</div>;
+};
 
 export default Message;
