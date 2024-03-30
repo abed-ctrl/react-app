402c6e29f4ba8ca9147cca057956d2c649d43845
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-03-30 00:20:22 +0000

    05 - Managing Component State - Understanding the State Hook Part2

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,22 @@ function App() {
   const [isVisible, setVisibility] = useState(false);
   const handleClick = () => {
     setVisibility(true);
+    // if we add a log to see isVisible after setVisibility to true we will notice that isVisible have the old value false
+    console.log(isVisible);
+    // the update is not applied immediately. it going to be applied asynchronously meaning in the future
+    // this is done for performance reasons. because if we have many useState like this.
+    // setName('Ali')
+    // setAge('32')
+    // ......
+    // we can't update the component every useState. the update take effect when we finish the handleClick function.
   };
+  // after handleClick React update all.
 
   return (
     <div>
       <button onClick={handleClick}>Show</button>
+      {/* in console you will see false and here you will see true because the handleClick finished*/}
+      <p>{String(isVisible)}</p>
     </div>
   );
 }
