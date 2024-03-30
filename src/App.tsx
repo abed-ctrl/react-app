eeb6eace5e9576da9f4fc3023e4a70ed793c0dd3
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-03-30 00:32:06 +0000

    05 - Managing Component State - Understanding the State Hook Part3

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,27 @@
 import { useState } from "react";
 
 function App() {
-  // 1- React update state asynchronously
+  // 2- State is stored outside of components
 
   const [isVisible, setVisibility] = useState(false);
+  // if we declare a variable
+  let count = 0;
+  // this is a scooped variable
+  // every time we call App component the variable count will initialize to 0
+  // this is why we use state hook to store the state outside of this component
+  // for some where React will store all the states variables for this component.
+  // and React automaticly remove this variables if the component is no longer visible in the screen.
+
   const handleClick = () => {
     setVisibility(true);
-    // if we add a log to see isVisible after setVisibility to true we will notice that isVisible have the old value false
+    // this update will be lost
+    count++;
     console.log(isVisible);
-    // the update is not applied immediately. it going to be applied asynchronously meaning in the future
-    // this is done for performance reasons. because if we have many useState like this.
-    // setName('Ali')
-    // setAge('32')
-    // ......
-    // we can't update the component every useState. the update take effect when we finish the handleClick function.
   };
-  // after handleClick React update all.
 
   return (
     <div>
       <button onClick={handleClick}>Show</button>
-      {/* in console you will see false and here you will see true because the handleClick finished*/}
-      <p>{String(isVisible)}</p>
     </div>
   );
 }
